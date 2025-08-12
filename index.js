bc47ac134a8de2bd52e359f451c11b37afbc01a3
TrilochanSahoo <trilochansahoo026@gmail.com>
2025-08-12 16:01:44 +0530

    added api changes

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express")
 const cors = require("cors")
 const app = express()
 const apiRoutes = require('./src/api/v1/routes')
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api",apiRoutes)
 
--- src/api/v1/controller/tokenGenerator.js
+++ src/api/v1/controller/tokenGenerator.js
@@ -1,7 +1,115 @@
-const tokenGenerator = (req,res) => {
-    res.status(200).json({
-        data : "message"
+function tokenize(text){
+  const tokens = [];
+  let i = 0;
+//   console.log(text.length);
+
+  while (i < text.length) {
+    let char = text[i];
+
+    // Whitespace
+    if (/\s/.test(char)) {
+      let start = i;
+      while (i < text.length && /\s/.test(text[i])) {
+        i++;
+      }
+      let space = text.slice(start, i);
+      tokens.push(space);
+      continue;
+    }
+
+    // Numbers (integer or decimal)
+    if (/\d/.test(char)) {
+      let start = i;
+      while (i < text.length && (/\d/.test(text[i]) || (text[i] === "." && /\d/.test(text[i + 1])))) {
+        i++;
+      }
+      let num = text.slice(start, i);
+      tokens.push(num);
+      continue;
+    }
+
+    // Words (max 5 letter)
+    if (/[a-zA-Z]/.test(char)) {
+        let start = i;
+        while (i < text.length && /[a-zA-Z]/.test(text[i])) {
+            i++;
+        }
+        let word = text.slice(start, i);
+
+        for (let j = 0; j < word.length; j += 5) {
+            tokens.push(word.slice(j, j + 5));
+        }
+
+        continue;
+    }
+
+    // Special character
+    tokens.push(char);
+    i++;
+  }
+
+  return tokens;
+}
+
+function encodedTokenFun(tokens){
+    const encodedToken = []
+    tokens.forEach(str => {
+        encodedToken.push(str.split("").map(ch => String(ch.charCodeAt(0)).padStart(3, "0")).join(""));
+    });
+    return encodedToken
+    
+}
+
+function decodedTokenFun(tokens){
+    const decodedToken = []
+    tokens.forEach((ele) =>{
+        let result = "";
+        for (let i = 0; i < ele.length; i += 3) {
+            result += String.fromCharCode(parseInt(ele.slice(i, i + 3), 10));
+        }
+        decodedToken.push(result)
     })
+    return decodedToken
+}
+
+
+// Note : add encoded element array to console.log the decoded tokens
+// console.log(decodedTokenFun([
+//     "116069115116",
+//     "032032032032",
+//     "116101115116",
+//     "049050051049050051",
+//     "032",
+//     "049",
+//     "032",
+//     "102097115097100",
+//     "102097115100102",
+//     "115097100102115",
+//     "100097102115097",
+//     "100102097115102"
+//   ]))
+
+
+const tokenGenerator = (req,res) => {
+    try {
+        const input = req.body.input
+        if (typeof input !== "string") {
+            return res.status(400).json({
+                error: "Input must be a string"
+            });
+        }
+        const tokens = tokenize(input)
+        const encodedTokens = encodedTokenFun(tokens)
+    
+        res.status(200).json({
+            tokens : tokens,
+            encodedTokens : encodedTokens
+        })
+    } catch (error) {
+        res.status(500).json({
+            error: "Internal server error while generating tokens"
+        });
+    }
 }
 
 module.exports = tokenGenerator
